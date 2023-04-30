314a391f47a9fe5458e2d075d9dd545e2204beb3
tstavinoha <tin.stavinoha@infobip.com>
2023-05-01 00:20:31 +0200

    Fixed tests :)

--- testing-a2z-service/src/test/java/com/testing/a2z/IntegrationTestBase.java
+++ testing-a2z-service/src/test/java/com/testing/a2z/IntegrationTestBase.java
@@ -1,8 +1,21 @@
 package com.testing.a2z;
 
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.repository.CrudRepository;
 
 @SpringBootTest
 public class IntegrationTestBase {
+
+    @Autowired
+    List<CrudRepository<?, ?>> allRepositories;
+
+    @AfterEach
+    public void cleanUp() {
+        allRepositories.forEach(CrudRepository::deleteAll);
+    }
 
 }
